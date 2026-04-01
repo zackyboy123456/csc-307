0f5866fd622a877da7ad15c2dd167d0e32c10725
Zachary Harder <zharder7@gamil.com>
2026-04-01 13:41:47 -0700

    final commit for lab 1 implment the adding of names and jobs

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,25 +1,14 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([
-  {
-    name: "Charlie",
-    job: "Janitor"
-  },
-  {
-    name: "Mac",
-    job: "Bouncer"
-  },
-  {
-    name: "Dee",
-    job: "Aspring actress"
-  },
-  {
-    name: "Dennis",
-    job: "Bartender"
-  }
+  
 ]);
+  function updateList(person) {
+  setCharacters([...characters, person]);
+  }
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
@@ -32,6 +21,7 @@ function MyApp() {
       characterData={characters}
       removeCharacter={removeOneCharacter}
     />
+    <Form handleSubmit={updateList} />
   </div>
 );
 }
