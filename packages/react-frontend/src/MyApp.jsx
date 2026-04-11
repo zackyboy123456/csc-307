0fc13f876e315f631981bb9cbf0b3bea1dd524ed
Zachary Harder <zharder7@gamil.com>
2026-04-11 14:23:16 -0700

    finised firs question now returns 201

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import Table from "./Table";
 import Form from "./Form";
 
@@ -6,8 +6,33 @@ function MyApp() {
   const [characters, setCharacters] = useState([
   
 ]);
-  function updateList(person) {
-  setCharacters([...characters, person]);
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+  useEffect(() => {
+  fetchUsers()
+	  .then((res) => res.json())
+	  .then((json) => setCharacters(json["users_list"]))
+	  .catch((error) => { console.log(error); });
+  }, [] );
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+  function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
   }
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
